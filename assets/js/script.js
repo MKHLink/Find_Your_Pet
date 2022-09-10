666f9e47745b9ffe09055e549616adceb66281c8
Aleph Null <likhonhasankomool@gmail.com>
2022-09-10 15:56:28 -0400

    fixed script tag in index and got api working with token and can search using hardcoded parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,50 @@
 var apiKey = "gSzfuztB3Wmmxgv5n0Q6rR4ty7EtEc14eez9SiOYgZHameqeHU";
 var secretKey = "W6jfFnFl2Jxqo2bUSnC6s90C1PzCF4Fv6K8SyHGp";
 
-getToken();
+console.log("Test");
+
 function getToken()
 {
-   fetch('curl -d "grant_type=client_credentials&client_id=gSzfuztB3Wmmxgv5n0Q6rR4ty7EtEc14eez9SiOYgZHameqeHU&client_secret=W6jfFnFl2Jxqo2bUSnC6s90C1PzCF4Fv6K8SyHGp" https://api.petfinder.com/v2/oauth2/token')
-   .then(function(response){
+    var accessToken;
+
+    fetch('https://api.petfinder.com/v2/oauth2/token', 
+    {
+        method: 'POST',
+        body: new URLSearchParams
+    ({
+        'grant_type': 'client_credentials',
+        'client_id': 'gSzfuztB3Wmmxgv5n0Q6rR4ty7EtEc14eez9SiOYgZHameqeHU',
+        'client_secret': 'W6jfFnFl2Jxqo2bUSnC6s90C1PzCF4Fv6K8SyHGp'
+    })
+    }).then(function(response){
+        response.json().then(function(data){
+        console.log(data);
+        accessToken =  data.access_token;
+        console.log(accessToken);
+
+        fetch('https://api.petfinder.com/v2/animals?type=dog', {
+            headers: {
+            'Authorization': 'Bearer '+data.access_token
+            }
+        }).then(function(response){
+            response.json().then(function(data){
+                console.log(data);
+            });
+        });
+    });
+    
+    
+});
+    
+}
+
+getToken();
+
+//curl -d "grant_type=client_credentials&client_id=gSzfuztB3Wmmxgv5n0Q6rR4ty7EtEc14eez9SiOYgZHameqeHU&client_secret=W6jfFnFl2Jxqo2bUSnC6s90C1PzCF4Fv6K8SyHGp" https://api.petfinder.com/v2/oauth2/token
+
+/*
+.then(function(response){
     response.json().then(function(data){
         console.log(data);
     });
-   });
-}
-
-
-//curl -d "grant_type=client_credentials&client_id=gSzfuztB3Wmmxgv5n0Q6rR4ty7EtEc14eez9SiOYgZHameqeHU&client_secret=W6jfFnFl2Jxqo2bUSnC6s90C1PzCF4Fv6K8SyHGp" https://api.petfinder.com/v2/oauth2/token
+*/
